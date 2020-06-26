f097dd3ae7ca1e355016c16afe57e17c43dd9f94
Marcos Hobson Souza Leite <marcos.leite@alubar.net>
2020-06-26 15:40:07 -0300

    Configurando o projeto

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -58,7 +58,7 @@ $autoload['packages'] = array();
 |
 |	$autoload['libraries'] = array('user_agent' => 'ua');
 */
-$autoload['libraries'] = array('session', 'encryption', 'database', 'my_script_helper');
+$autoload['libraries'] = array('session', 'encryption', 'database');
 
 /*
 | -------------------------------------------------------------------
@@ -89,7 +89,7 @@ $autoload['drivers'] = array();
 |
 |	$autoload['helper'] = array('url', 'file');
 */
-$autoload['helper'] = array('html', 'url', 'text', 'form');
+$autoload['helper'] = array('html', 'url', 'text', 'form', 'my_script_helper');
 
 /*
 | -------------------------------------------------------------------
