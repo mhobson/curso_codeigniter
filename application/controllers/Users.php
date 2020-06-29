ac4633d1f9d094773a5a8c729c40b1566eb40c9c
Marcos Hobson Souza Leite <marcos.leite@alubar.net>
2020-06-29 15:38:32 -0300

    Criando a função de cadastro de usuário new_user

--- application/controllers/Users.php
+++ application/controllers/Users.php
@@ -12,7 +12,7 @@ class Users extends CI_Controller
 	// index page
 	public function index()
 	{
-		//state and city
+		// state and city
 		$data['states'] = $this->db->get('states')->result();
 		$data['cities'] = $this->db->get('cities')->result();
 
@@ -27,5 +27,32 @@ class Users extends CI_Controller
 		$this->load->view(__FUNCTION__);
 		$this->load->view('footer');
 		$this->load->view('html_footer');
+	}
+
+	public function new_user()
+	{
+		// get from form
+		$name = $this->input->post('name');
+		$email = $this->input->post('email');
+		$birth = $this->input->post('birth');
+		$cpf = $this->input->post('cpf');
+		$gender = $this->input->post('gender');
+		$state = $this->input->post('state');
+		$city = $this->input->post('city');
+		$password = $this->input->post('password');
+
+		$data = array(
+			'name' => $name,
+			'email' => $email,
+			'birth' => $birth,
+			'cpf' => $cpf,
+			'gender' => $gender,
+			'state' => $state,
+			'city' => $city,
+			'password' => $password
+		);
+
+		echo "<pre>";
+		print_r($data);
 	}
 }
